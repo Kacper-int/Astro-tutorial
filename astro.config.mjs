76b2888f42ea05176a91e9bcd461924833caa738
Kacper-int <kacper.gorzen@gmail.com>
2026-01-12 09:04:33 +0100

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://elegant-yeot-ad658ea.netlify.app/"
+  site: "https://elegant-yeot-ad658ea.netlify.app/",
+  integrations: [preact()]
 });
